bd4a9c20c45887e72fe44acab00a71dfade452b1
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-09-06 03:39:31 -0400

    added mouse controller

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene() //holds all object including cameras and lights
 
@@ -16,10 +17,22 @@ camera.position.setZ(30) //moves camera away from center
 renderer.render(scene, camera) //draws
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100) //creates object in scene
-const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true}) //can be considered a "wrapping paper" for the geometry
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347}) //can be considered a "wrapping paper" for the geometry
 const torus = new THREE.Mesh(geometry, material) //combines shape and material to create the mesh
 
 scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff) //let there be light
+pointLight.position.set(5, 5, 5) //moves light away from center
+
+const ambientLight = new THREE.AmbientLight(0xffffff)// more like a floodlight
+scene.add(pointLight, ambientLight) //adds light to scene
+
+const lightHelper = new THREE.PointLightHelper(pointLight)// adds shape aroung light
+const gridHelper = new THREE.GridHelper(200, 50)// creates grid
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement) //listens to mouse controls and moves camera accordingly
 
 function animate(){
   //recursivley rerenders the scene
@@ -28,6 +41,8 @@ function animate(){
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
   torus.rotation.z += 0.01
+
+  controls.update()
 
   renderer.render(scene, camera)
 }
