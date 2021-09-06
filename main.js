9e76df65a85f95858f6d7dea177f8634fe2f1dcd
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-09-06 03:25:36 -0400

    created scene and renderer

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 import './style.css'
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene() //holds all object including cameras and lights
+
+const aspectRatio = window.innerWidth / window.innerHeight
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000) //camera
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+}) //makes the magic happen
+renderer.setPixelRatio(window.devicePixelRatio) //sets pixel ratio to device pixel ratio
+renderer.setSize(window.innerWidth, window.innerHeight) //makes renderer full screen
+camera.position.setZ(30) //moves camera away from center
+
+renderer.render(scene, camera) //draws
