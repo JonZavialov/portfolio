2ca19a2b0e20b80c43b1c4e9ac95343189a84a77
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-09-06 18:11:31 -0400

    completed js and added html template

--- main.js
+++ main.js
@@ -34,6 +34,64 @@ scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement) //listens to mouse controls and moves camera accordingly
 
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('./images/space.jpg') //ads background
+scene.background = spaceTexture
+
+const jonTexture = new THREE.TextureLoader().load('./images/jon.png')
+const jon = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial({map: jonTexture})
+)
+scene.add(jon)
+
+const moonTexture = new THREE.TextureLoader().load('./images/moon.jpg')
+const normalTexture = new THREE.TextureLoader().load('./images/normal.jpg')
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture
+  })
+)
+scene.add(moon)
+
+moon.position.z = 30 //same thing
+moon.position.setX(-10) //same thing
+
+jon.position.z = -5
+jon.position.x = 2
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top //gets current area where user is scrolled to
+   
+  moon.rotation.x += 0.05
+  moon.rotation.y += 0.075
+  moon.rotation.z += 0.05
+
+  jon.rotation.y += 0.01
+  jon.rotation.z += 0.01
+
+  camera.position.z = t * -0.01
+  camera.position.x = t * -0.0002
+  camera.rotation.y = t * -0.0002
+}
+
+document.body.onscroll = moveCamera //sets movecamera method to onscroll event
+moveCamera()
+
+
 function animate(){
   //recursivley rerenders the scene
   requestAnimationFrame(animate)
@@ -42,7 +100,7 @@ function animate(){
   torus.rotation.y += 0.005
   torus.rotation.z += 0.01
 
-  controls.update()
+  //controls.update()
 
   renderer.render(scene, camera)
 }
